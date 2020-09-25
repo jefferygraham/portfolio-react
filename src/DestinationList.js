932b185a70dbadcfc6db7069a37c3c3fe241d6d7
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-09-24 22:38:40 -0400

    add reactstrap Card component

--- src/DestinationList.js
+++ src/DestinationList.js
@@ -1,86 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Card, CardBody, CardImg, CardTitle } from 'reactstrap';
 
-function DestinationList() {
-  return (
-    <div className='container px-5 py-5'>
-      <div className='row flex-column align-items-center'>
-        <h2 className='display-4 heading'>Popular Destinations</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
-          necessitatibus.
-        </p>
-      </div>
-      {/* <!-- CARD DECK --> */}
-      <div className='row pt-5'>
-        <div className='card-deck'>
-          <div className='card border-0'>
-            <img
-              className='card-img-top rounded-0'
-              src='/assets/images/paris.jpg'
-              alt='Paris'
-            />
-            <div className='card-body pl-0'>
-              <h5 className='card-title'>Paris</h5>
+const destinations = [
+  {
+    title: 'Paris',
+    src: '/assets/images/paris.jpg',
+    altText: 'Paris',
+  },
+  {
+    title: 'Thailand',
+    src: '/assets/images/thailand.jpg',
+    altText: 'Thailand',
+  },
+  {
+    title: 'Greece',
+    src: '/assets/images/greece.jpg',
+    altText: 'Greece',
+  },
+  {
+    title: 'Australia',
+    src: '/assets/images/australia.jpg',
+    altText: 'Paris',
+  },
+  {
+    title: 'Cape Town',
+    src: '/assets/images/cape-town.jpg',
+    altText: 'Cape Town',
+  },
+  {
+    title: 'Rome',
+    src: '/assets/images/rome.jpg',
+    altText: 'Rome',
+  },
+];
+
+class DestinationList extends Component {
+  render() {
+    return (
+      <div className='container px-5 py-5'>
+        <div className='row flex-column align-items-center'>
+          <h2 className='display-4 heading'>Popular Destinations</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
+            necessitatibus.
+          </p>
+        </div>
+        {/* <!-- CARD DECK --> */}
+        <div className='row pt-5 text-center'>
+          {destinations.map((destination) => (
+            <div className='col-4'>
+              <Card className='mb-3 border-0'>
+                <CardImg
+                  top
+                  className='shadow-lg'
+                  width='100%'
+                  src={destination.src}
+                  alt={destination.altText}
+                />
+                <CardBody>
+                  <CardTitle>{destination.title}</CardTitle>
+                </CardBody>
+              </Card>
             </div>
-          </div>
-          <div className='card border-0'>
-            <img
-              className='card-img-top rounded-0'
-              src='/assets/images/thailand.jpg'
-              alt='Thailand'
-            />
-            <div className='card-body pl-0'>
-              <h5 className='card-title'>Thailand</h5>
-            </div>
-          </div>
-          <div className='card border-0'>
-            <img
-              className='card-img-top rounded-0'
-              src='/assets/images/greece.jpg'
-              alt='Greece'
-            />
-            <div className='card-body pl-0'>
-              <h5 className='card-title'>Greece</h5>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
-      <div className='row'>
-        <div className='card-deck'>
-          <div className='card border-0'>
-            <img
-              className='card-img-top rounded-0'
-              src='/assets/images/australia.jpg'
-              alt='Australia'
-            />
-            <div className='card-body pl-0'>
-              <h5 className='card-title'>Australia</h5>
-            </div>
-          </div>
-          <div className='card border-0'>
-            <img
-              className='card-img-top rounded-0'
-              src='/assets/images/cape-town.jpg'
-              alt='Cape Town'
-            />
-            <div className='card-body pl-0'>
-              <h5 className='card-title'>Cape Town</h5>
-            </div>
-          </div>
-          <div className='card border-0'>
-            <img
-              className='card-img-top rounded-0'
-              src='/assets/images/rome.jpg'
-              alt='Rome'
-            />
-            <div className='card-body pl-0'>
-              <h5 className='card-title'>Rome</h5>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default DestinationList;
